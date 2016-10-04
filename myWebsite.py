0d187419fa57549b25aa91e5e9f311c552a069c4
Chris Jordan <jordanch@umich.edu>
2016-10-04 00:52:51 -0400

    finished first build

--- myWebsite.py
+++ myWebsite.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+from flask import *
+from flask import request
+from flask_mail import Mail, Message
 
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+mail = Mail(app)
 
+
+app.debug = True
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'user@gmail.com'
+app.config['MAIL_PASSWORD'] = 'password;'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 @app.route('/')
 def home():
@@ -20,8 +32,15 @@ def projects():
 def hobbies():
 	return render_template('hobbies.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+	if request.method=='POST':
+		name = request.form['name']
+		email = request.form['email']
+		message = request.form['message']
+		msg = Message(message, sender=email, recipients=["jordanch@umich.edu"])
+		msg.body = "This is the email body"
+		mail.send(msg)
 	return render_template('contact.html')
 
 if __name__=='__main__':
